4b02d719b46b97579a5947083568ce7ffef86642
kik007 <kik78118@gmail.com>
2017-07-19 11:19:03 +0800

    第二堂課範例更新

---
+++ Lecture02-Examples/PokemonFormsApp/PokemonFormsApp/PokemonForm.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace PokemonFormsApp
+{
+    partial class PokemonForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PokemonForm));
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.EvolveCandy_Label = new System.Windows.Forms.Label();
+            this.PowerUpCandy_Label = new System.Windows.Forms.Label();
+            this.PowerUpStardust_Label = new System.Windows.Forms.Label();
+            this.UserCandy_Label = new System.Windows.Forms.Label();
+            this.UserStardust_Label = new System.Windows.Forms.Label();
+            this.Height_Label = new System.Windows.Forms.Label();
+            this.Types_Label = new System.Windows.Forms.Label();
+            this.Weight_Label = new System.Windows.Forms.Label();
+            this.Hp_Label = new System.Windows.Forms.Label();
+            this.Monster_Label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(291, 250);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(30, 23);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 53;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(291, 221);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(30, 23);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 52;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(132, 221);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(30, 23);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 51;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(230, 177);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(30, 23);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 50;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(90, 177);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 23);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 49;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(171, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 48;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(51, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 47;
+            this.button2.Text = "進化";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 46;
+            this.button1.Text = "強化";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // EvolveCandy_Label
+            // 
+            this.EvolveCandy_Label.AutoSize = true;
+            this.EvolveCandy_Label.Location = new System.Drawing.Point(327, 255);
+            this.EvolveCandy_Label.Name = "EvolveCandy_Label";
+            this.EvolveCandy_Label.Size = new System.Drawing.Size(101, 12);
+            this.EvolveCandy_Label.TabIndex = 45;
+            this.EvolveCandy_Label.Text = "EvolveCandy_Label";
+            // 
+            // PowerUpCandy_Label
+            // 
+            this.PowerUpCandy_Label.AutoSize = true;
+            this.PowerUpCandy_Label.Location = new System.Drawing.Point(327, 226);
+            this.PowerUpCandy_Label.Name = "PowerUpCandy_Label";
+            this.PowerUpCandy_Label.Size = new System.Drawing.Size(111, 12);
+            this.PowerUpCandy_Label.TabIndex = 44;
+            this.PowerUpCandy_Label.Text = "PowerUpCandy_Label";
+            // 
+            // PowerUpStardust_Label
+            // 
+            this.PowerUpStardust_Label.AutoSize = true;
+            this.PowerUpStardust_Label.Location = new System.Drawing.Point(168, 226);
+            this.PowerUpStardust_Label.Name = "PowerUpStardust_Label";
+            this.PowerUpStardust_Label.Size = new System.Drawing.Size(117, 12);
+            this.PowerUpStardust_Label.TabIndex = 43;
+            this.PowerUpStardust_Label.Text = "PowerUpStardust_Label";
+            // 
+            // UserCandy_Label
+            // 
+            this.UserCandy_Label.AutoSize = true;
+            this.UserCandy_Label.Location = new System.Drawing.Point(266, 183);
+            this.UserCandy_Label.Name = "UserCandy_Label";
+            this.UserCandy_Label.Size = new System.Drawing.Size(89, 12);
+            this.UserCandy_Label.TabIndex = 42;
+            this.UserCandy_Label.Text = "UserCandy_Label";
+            // 
+            // UserStardust_Label
+            // 
+            this.UserStardust_Label.AutoSize = true;
+            this.UserStardust_Label.Location = new System.Drawing.Point(126, 183);
+            this.UserStardust_Label.Name = "UserStardust_Label";
+            this.UserStardust_Label.Size = new System.Drawing.Size(95, 12);
+            this.UserStardust_Label.TabIndex = 41;
+            this.UserStardust_Label.Text = "UserStardust_Label";
+            // 
+            // Height_Label
+            // 
+            this.Height_Label.AutoSize = true;
+            this.Height_Label.Location = new System.Drawing.Point(315, 141);
+            this.Height_Label.Name = "Height_Label";
+            this.Height_Label.Size = new System.Drawing.Size(68, 12);
+            this.Height_Label.TabIndex = 40;
+            this.Height_Label.Text = "Height_Label";
+            // 
+            // Types_Label
+            // 
+            this.Types_Label.AutoSize = true;
+            this.Types_Label.Location = new System.Drawing.Point(183, 141);
+            this.Types_Label.Name = "Types_Label";
+            this.Types_Label.Size = new System.Drawing.Size(65, 12);
+            this.Types_Label.TabIndex = 39;
+            this.Types_Label.Text = "Types_Label";
+            // 
+            // Weight_Label
+            // 
+            this.Weight_Label.AutoSize = true;
+            this.Weight_Label.Location = new System.Drawing.Point(49, 141);
+            this.Weight_Label.Name = "Weight_Label";
+            this.Weight_Label.Size = new System.Drawing.Size(71, 12);
+            this.Weight_Label.TabIndex = 38;
+            this.Weight_Label.Text = "Weight_Label";
+            // 
+            // Hp_Label
+            // 
+            this.Hp_Label.AutoSize = true;
+            this.Hp_Label.Location = new System.Drawing.Point(194, 117);
+            this.Hp_Label.Name = "Hp_Label";
+            this.Hp_Label.Size = new System.Drawing.Size(51, 12);
+            this.Hp_Label.TabIndex = 37;
+            this.Hp_Label.Text = "Hp_Label";
+            // 
+            // Monster_Label
+            // 
+            this.Monster_Label.AutoSize = true;
+            this.Monster_Label.Location = new System.Drawing.Point(183, 93);
+            this.Monster_Label.Name = "Monster_Label";
+            this.Monster_Label.Size = new System.Drawing.Size(75, 12);
+            this.Monster_Label.TabIndex = 36;
+            this.Monster_Label.Text = "Monster_Label";
+            // 
+            // PokemonForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 289);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.EvolveCandy_Label);
+            this.Controls.Add(this.PowerUpCandy_Label);
+            this.Controls.Add(this.PowerUpStardust_Label);
+            this.Controls.Add(this.UserCandy_Label);
+            this.Controls.Add(this.UserStardust_Label);
+            this.Controls.Add(this.Height_Label);
+            this.Controls.Add(this.Types_Label);
+            this.Controls.Add(this.Weight_Label);
+            this.Controls.Add(this.Hp_Label);
+            this.Controls.Add(this.Monster_Label);
+            this.Name = "PokemonForm";
+            this.Text = "PokemonForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label EvolveCandy_Label;
+        private System.Windows.Forms.Label PowerUpCandy_Label;
+        private System.Windows.Forms.Label PowerUpStardust_Label;
+        private System.Windows.Forms.Label UserCandy_Label;
+        private System.Windows.Forms.Label UserStardust_Label;
+        private System.Windows.Forms.Label Height_Label;
+        private System.Windows.Forms.Label Types_Label;
+        private System.Windows.Forms.Label Weight_Label;
+        private System.Windows.Forms.Label Hp_Label;
+        private System.Windows.Forms.Label Monster_Label;
+    }
+}
+
---
+++ Lecture02-Examples/PokemonFormsApp/PokemonFormsApp/PokemonForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MonsterLibrary;
+
+namespace PokemonFormsApp
+{
+    public partial class PokemonForm : Form
+    {
+        public Monster Monster;
+        public int UserStarDust;
+        public int UserCandy;
+
+        public PokemonForm()
+        {
+            InitializeComponent();
+            UserStarDust = 100000;
+            UserCandy = 1234;
+            Monster = new Monster()
+            {
+                Name = "妙蛙種子",
+                CurrentHp = 40,
+                Hp = 60,
+                Weight = 6.94f,
+                Types = new string[] { "草", "毒" },
+                Height = 0.66f,
+                PowerUpCandy = 3,
+                PowerUpStardust = 3000,
+                EvolveCandy = 25
+            };
+            Monster_Label.Text = Monster.Name;
+            Hp_Label.Text = Monster.CurrentHp.ToString() + "/" + Monster.Hp + "HP";
+            Height_Label.Text = Monster.Height.ToString() + "m";
+            Types_Label.Text = Monster.Types[0] + "/" + Monster.Types[1];
+            Weight_Label.Text = Monster.Weight.ToString() + "kg";
+            PowerUpCandy_Label.Text = Monster.PowerUpCandy.ToString();
+            PowerUpStardust_Label.Text = Monster.PowerUpStardust.ToString();
+            EvolveCandy_Label.Text = Monster.EvolveCandy.ToString();
+            UserStardust_Label.Text = UserStarDust.ToString();
+            UserCandy_Label.Text = UserCandy.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UserCandy = UserCandy - Monster.PowerUpCandy;
+            UserStarDust = UserStarDust - Monster.PowerUpStardust;
+            Monster.Hp = Monster.Hp + 10;
+            Monster.CurrentHp = Monster.CurrentHp + 10;
+            Hp_Label.Text = Monster.CurrentHp.ToString() + "/" + Monster.Hp + "HP";
+            UserStardust_Label.Text = UserStarDust.ToString();
+            UserCandy_Label.Text = UserCandy.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            UserCandy = UserCandy - Monster.EvolveCandy;
+            Monster = new Monster()
+            {
+                Name = "妙蛙草",
+                CurrentHp = 150,
+                Hp = 250,
+                Weight = 10.94f,
+                Types = new string[] { "草", "毒" },
+                Height = 1.66f,
+                PowerUpCandy = 3,
+                PowerUpStardust = 3000,
+                EvolveCandy = 100
+            };
+            Monster_Label.Text = Monster.Name;
+            Hp_Label.Text = Monster.CurrentHp.ToString() + "/" + Monster.Hp + "HP";
+            Height_Label.Text = Monster.Height.ToString();
+            Types_Label.Text = Monster.Types[0] + "/" + Monster.Types[1];
+            Weight_Label.Text = Monster.Weight.ToString();
+            PowerUpCandy_Label.Text = Monster.PowerUpCandy.ToString();
+            PowerUpStardust_Label.Text = Monster.PowerUpStardust.ToString();
+            EvolveCandy_Label.Text = Monster.EvolveCandy.ToString();
+            UserStardust_Label.Text = UserStarDust.ToString();
+            UserCandy_Label.Text = UserCandy.ToString();
+        }
+    }
+}
