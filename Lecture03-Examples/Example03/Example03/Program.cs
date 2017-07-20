6baeb5a2092be3a764789827887b767928180964
kik007 <kik78118@gmail.com>
2017-07-20 11:40:18 +0800

    第三堂課範例更新

---
+++ Lecture03-Examples/Example03/Example03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example03
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Calculator iphone = new Calculator();
+            int[] numbers = new int[] { 1, 3, 19 };
+            int result = iphone.Sum(numbers);
+            Console.WriteLine(result);
+
+            Person elsa = new Person()
+            {
+                Name = "Elsa",
+                Pos = new Position()
+                {
+                    X = 0,
+                    Y = 0
+                },
+                Angle = 0
+            };
+
+            for (int count = 0; count < 3; count++)
+            {
+                elsa.DrawSquare(100);
+                elsa.TrueRight(120);
+            }
+        }
+    }
+}
